9bf3aca1b8b209a25b9b59f24b5a478910993cdc
Justin <zertukis@gmail.com>
2015-08-22 16:35:45 -0500

    some work on battle controller

--- src/client/app/battle/battle.controller.js
+++ src/client/app/battle/battle.controller.js
@@ -10,12 +10,30 @@
     /* @ngInject */
     function BattleController($rootScope) {
         var vm = this;
+
+        vm.level = '__________________';
+
+
+
+
         activate();
+
+
+        function moveInLevel(i) {
+            var player = "Y";
+            console.log(vm.level.split());
+            if (vm.level[i] == '_') {
+                console.log('floor');
+            }
+        }
+
 
         ////////////////
 
         function activate() {
-            
+            for (var i = 0; i < vm.level.length; i++) {
+                moveInLevel(i);
+            }
         }
     }
 })();

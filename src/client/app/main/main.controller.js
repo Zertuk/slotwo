24f683e8117fa42fa37c141d9c053ccbba77c1a5
Justin <zertukis@gmail.com>
2015-10-11 18:54:35 -0500

    can now add clickable items to maps properly

--- src/client/app/main/main.controller.js
+++ src/client/app/main/main.controller.js
@@ -5,28 +5,33 @@
         .module('app.main')
         .controller('MainController', MainController);
 
-    MainController.$inject = ['$rootScope', 'playerService', 'mainService'];
+    MainController.$inject = ['$rootScope', 'playerService', 'mainService', '$timeout', '$sce'];
 
     /* @ngInject */
-    function MainController($rootScope, playerService, mainService) {
+    function MainController($rootScope, playerService, mainService, $timeout, $sce) {
         var vm = this;
         vm.title = 'MainController';
         vm.count = 0;
         vm.player = playerService.player;
+        vm.currentLocation = mainService.treeCity;
 
-        vm.currentLocation = 
-
+        function sanitizeAscii() {
+            for (var i = 0; i < vm.currentLocation.ascii.length; i++) {
+                vm.currentLocation.ascii[i] = $sce.trustAsHtml(vm.currentLocation.ascii[i]);
+            }
+        }
         activate();
 
         ////////////////
 
         function activate() {
+            sanitizeAscii();
             mainLoop();
         }
 
         function mainLoop() {
             vm.count = vm.count + 1;
-            setTimeout(mainLoop, 1000);
+            $timeout(mainLoop, 1000);
         }
     }
 })();
--- src/client/app/main/main.service.js
+++ src/client/app/main/main.service.js
@@ -9,13 +9,29 @@
 
     /* @ngInject */
     function mainService() {
-        this.Location = function() {
+    	var vm = this;
+    	vm.setCharAt = function(str,index,chr) {
+            if(index > str.length-1) return str;
+            return str.substr(0,index) + chr + str.substr(index+1);
+        }
+        vm.click = function(ascii, data,  x, y, xlength, ylength) {
+            for (var i = 0; i < ylength; i++) {
+            	console.log(data);
+                var before = '<span class = "click" ng-model = "' + data + '">' + ascii[y + i][x];
+                var after = ascii[y + i][x + xlength] + '</span>';
+                //after then before to not break order!
+                ascii[y + i] = this.setCharAt(ascii[y + i], x + xlength, after);
+                ascii[y + i] = this.setCharAt(ascii[y + i], x, before);
+            }
+        }
+
+        vm.Location = function() {
     		
         };
 
-        this.treeCity = new this.Location;
-        this.treeCity.name = 'Tree City';
-		this.treeCity.ascii = ["        ///////\\\\\\\\\\\\\\         //////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\ ///////\\\\\\\\\\\\\\             |       |           |              ",                                                                        
+        vm.treeCity = new vm.Location;
+        vm.treeCity.name = 'Tree City';
+		vm.treeCity.ascii = ["        ///////\\\\\\\\\\\\\\         //////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\ ///////\\\\\\\\\\\\\\             |       |           |              ",                                                                        
 							   "      /////////\\\\\\\\\\\\\\\\\\     ////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\//////\\\\\\\\\\\\              |       |           |              ",                                                                        
 							   "       ////////\\\\\\\\\\\\\\              |                   |    ///////\\\\\\\\\\\\\\             |       |     |     |              ",                                                                 
 							   "    ///////////\\\\\\\\\\\\\\\\\\\\           |      |            |    ///////\\\\\\\\\\\\\\\\      .     |   |         |     |              ",                                                               
@@ -27,7 +43,7 @@
 							   "        |    _____   |   /  \\ / \\ |      _____            |     |  ||  |    |         /        /:___:\\     |   \\     |     ",                                                                        
 							   "        |   |     |  |   /  \\ / \\ |     /     \\           |                      ...''        /:_____:\\    |    ''...      ",                                                                          
 							   "________|___|     |__|____||___|__|_____|     |___________|___________________.''            /:_______:\\             ''.   ",                                                                           
-							   "	                                                                                             :_________:            '. ",                                                                   
+							   "                                                                                             :_________:              '. ",                                                                   
 							   "____________________________________________________________________  ____________________________________________________\\",                                                                    
 							   "                                                                    \\ \\____                                                ",                                                                         
 							   "                      ______________             ______________      \\____ \\                   .                           ",                                                                        
@@ -37,8 +53,16 @@
 							   "                 :     / \\     / \\      :   : / \\  / \\   .'''....' :             \\_____________________________________|___",                                                                            
 							   "                 |     / \\      |       |   | / \\   |  .'          |                                                       ",                                                                        
 							   "                 |      |               |   |  |      ;            |                                                       "]                                                                        
+		vm.treeCity.initClicks = function() {
+			vm.click(this.ascii, 'test', 92, 6, 12, 7);
+			vm.click(this.ascii, 'test', 40, 9, 7, 3);
+			vm.click(this.ascii, 'test', 12, 9, 7, 3);
+		};
+		vm.treeCity.initClicks();
 
-
+		vm.treeShop = new vm.Location;
+		vm.treeShop.name = 'Tree Shop';
+		vm.treeShop.ascii = ['test location'];
 
 
     }

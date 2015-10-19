bc3709fb8c0c482ef26e0eb71a18119bc862cf07
Justin <zertukis@gmail.com>
2015-10-18 19:59:15 -0500

    implementing main map

--- src/client/app/main/main.service.js
+++ src/client/app/main/main.service.js
@@ -14,13 +14,13 @@
             if(index > str.length-1) return str;
             return str.substr(0,index) + chr + str.substr(index+1);
         }
-        vm.click = function(ascii, data,  x, y, xlength, ylength) {
+        vm.click = function(location, hover, data,  x, y, xlength, ylength) {
             for (var i = 0; i < ylength; i++) {
-                var before = '<span class = "click" ng-click = "vm.switchLocation(\'' + data + '\')">' + ascii[y + i][x];
-                var after = ascii[y + i][x + xlength] + '</span>';
+                var before = '<span title = "' + hover + '" class = "click" ng-click = "vm.switchLocation(\'' + data + '\')">' + location.ascii[y + i][x];
+                var after = location.ascii[y + i][x + xlength] + '</span>';
                 //after then before to not break order!
-                ascii[y + i] = this.setCharAt(ascii[y + i], x + xlength, after);
-                ascii[y + i] = this.setCharAt(ascii[y + i], x, before);
+                location.ascii[y + i] = this.setCharAt(location.ascii[y + i], x + xlength, after);
+                location.ascii[y + i] = this.setCharAt(location.ascii[y + i], x, before);
             }
         }
 
@@ -32,6 +32,57 @@
         };
 
         vm.locationDictionary = [];
+
+        vm.mainMap = new vm.Location;
+        vm.mainMap.name = 'Map';
+
+		vm.mainMap.ascii = [	"            .                                  /              \\     /   \\                                                    /         \\   .'                         \\                  ",
+								"           /_\\'.                             .'   .^.         /\\   /     \\     ^                                           .'     ^  .  '. '.                          |         \\_      ",
+								"          /___\\/'.                          /    .   \\    ^ .'  '.'       '. .' '.             .......                    /      / .' '.  \\  :                          \\        /__     ",
+								"         /_____\\/ '.            +               /     \\  / \\   .''.         /     \\         ,''       '',                    /   ,'./ /     \\     ':   ~                 \\______/        ",
+								"   +    /_______\\ /             +            ../       .'   './    \\       /       \\       '.           .'             ,'   /    /       \\ ^    '.                       /      \\___/    ",
+								"   +   /   |~|   \\                         .'         /       \\     '.    ^        .^.       '..     ..'              /   ,'    /        .' '.    '.            ~    ___/       /   \\    ",
+								"                     +                    /          '         ' ^    \\  / \\      /   \\         '''''                /   /    .'      ^  /     \\     :            __/          /         ",
+								"                     +                              /          .' '.   .'   '.  .'     '.                           /        /      . ./       '.    '.          /                       ",
+								"   '                                               /          /     \\ /    ^  \\/ ^   ^   \\                .^.               :  .^. /   \\         \\     :        |                        ",
+								"                                   .             .'          /       '.   / \\  \\/ \\ / \\   :             .'   '.      .^.    / /   \\     \\         \\   ,'       /                         ",
+								"   ,              ^^                            /          .'          \\.'   './   ',  \\   \\    .^.  . /       \\ .^./   \\   .'     '.    '.          .'      _/                          ",
+								"                                                 ,,,,    .'            /       \\   .'.  '.     /   \\/ \\         /   \\    \\,'         \\      \\       :       /                            ",
+								"           .......                +             /    \\....            /       ^ ' /   \\  .\\  .'     \\.^.       /     \\   /.           '.          .'`  ~    |                            ",
+								"          /       \\               +             .... /    \\         .'       / \\.'     \\/ \\ /       /   \\    ,'       ','/ \            \\         :        /     \\___/                   ",
+								"  .'''''''.                                    /    \\                      .'  /     /\\/   \\   .'..'     \\  /  .'.    /,'   '..          ''\\     ,'        |_____/_                      ",
+								" /         \\                           ^        .                         /   /     /  \\    './           ''../   \\ .'/        '\\              .'        _/        \\__                   ",
+								"                                      / \\    . / \\                          .'    .'    \\    /               /    _'./           '.         ,''       __/         _/                     ",
+								"                                     ./ \\ . / \\/ \\                         /     /       ''./              .'    /_\\ \\                    ,'       __/                                   ",
+								"                  .              ^  / \\| / \\/ \\ |  .                            /         .'             ./           \\..           ..''''     ~  /                                      ",
+								"                                / \\ / \\  / \\ |    / \\ .                                                  /               \\        .'         ____/                                       ",
+								"^   .                        ^  / \\  |  /\\|    ^  / \\/ \\                                                                         :          /                                            ",
+								" \\ / \\  .                ^  / \\  |.    //\\\\   / \\  | / \\                                                                        .'       __|                                               \\ ",
+								" \\ / \\ / \\  ^  . /\\  ^  / \\ / \\  / \\ ////\\\\\\\\ / \\     |  ^                                                                    ,'        /                                            /      \\_____",
+								"| ^ |  /\\  / \\/ \\ ^ / \\ / \\  | ^ / \\  /-/\\-\\   |    .   / \\          ''                                                      :  ~      |              __/                            \\      /",
+								" / \\  /  \\ / \\/ \\/ \\/ \\  |    / \\ | ////--\\\\\\\\   . / \\^ / \\ .                                               ,,             ,.'        /                 \\____/                        \\____/_____",
+								" / \\  /  \\  |  | / \\ ^\\  /\\   ^ \\   /////\\\\\\\\\\  / \\/ / \ | / \\                                                           ,'          /                     \\__                       __ /    /  ",
+								"\\ | .  ||^    ^   | / \\ /  \\ / \\  /////|  |\\\\\\\\\\/ \\ |/ \\ . / \\                                                         :'          _/                   ___/                           /__",
+								" \\ / \\  / \\  / \\  . / \\^/  \\ / \\  ^    |##|   /\\ |.   |^/ \\ |                                                        .'         __/                        \\__                        /",
+								" \\ / \\  / \\  / \\ / \\ |/ \\|| ^ |  / \\    ^    /  \\/ \\^ / \\ \\.             *               ,,,                        .'         /                                                          ",
+								"|   |    |    |  / \\  / \\  / \\ ^ / \\   / \\   /  \\/\\/ \\/ \\|/ \\                                                      :           |                                                           ",
+								"                  |    |   / \\/ \\ |  ^ / \\ ^  ||/  \\ \\ |  / \\                                                       '.   ~      \\___                                                      ",
+								"                            | / \\   / \\ | / \\   ^  \\|.  .  |                                                          ',       ~    \\                                                    ",
+								"   ,,,                ,,,      |  ^ / \\   / \\  / \\ _/ \\/ \\                                                             :            |                                                   ",
+								"                                 / \\ |^    |   / \\| / \\/ \\                                                  *           ',           \\                                                    ",    
+								"                                 / \\ / \\   ^    |   .|  |           '''                                                   ''.         \\_       \\__                                          ",        
+								"                            ,,    |  / \\  / \\   ^  / \\                                                                       :  ~       \\  \\___/                                        __               ", 
+								"                                      |   / \\  / \\ / \\                                                                      .'           \\__/__   \\__                                  /  \\             ",        
+								"                                           |   / \\  |                                                                      .'            /     \\__/                                 .''    ''.___           ",
+								"                                                |                                              ,,                       ,,:       ~  ___/    __/  \\__                              /        ___  '.________  ",        
+								"          ___                                                                                                         .;            /          \\                            ____.''    __  / , \\            ",
+								"         /\\__\\                 .                                                                                   ,''             /                                       /\"\\                            ",
+								"    .^.  ||  |                                                                                                     :              |                                        \\_________                        ",
+								"                                                                                                                  .'             /                                          ' ' ' ' ''.                       ",
+								"                               ,,,                                    ,,,                             *          ;              /                                             .__.__.__\\                     ",
+								"                  _                                                                                            .'         ~   .'                                                                              "];
+
+
 
         vm.treeCity = new vm.Location;
         vm.treeCity.name = 'Tree City';
@@ -47,7 +98,7 @@
 							   "        /       |    \\    /\\   .  /               |       \\ |   |  __  |   / \\        |        |     |        |     / \\    ",                                                                         
 							   "        |    _____   |   /  \\ / \\ |      _____            |     |  ||  |    |         /        /:___:\\     |   \\     |     ",                                                                        
 							   "        |   |     |  |   /  \\ / \\ |     /     \\           |                      ...''        /:_____:\\    |    ''...      ",                                                                          
-							   "________|___|     |__|____||___|__|_____|     |___________|___________________.''            /:_______:\\             ''.   ",                                                                           
+							   "________|___|     |__|____||___|__|_____|     |___________|___________________.''            /:_______:\\             '.   ",                                                                           
 							   "                                                                                             :_________:              '. ",                                                                   
 							   "____________________________________________________________________  ____________________________________________________\\",                                                                    
 							   "                                                                    \\ \\____                                                ",                                                                         
@@ -59,35 +110,39 @@
 							   "                 |     / \\      |       |   | / \\   |  .'          |                                                       ",                                                                        
 							   "                 |      |               |   |  |      ;            |                                                       "]                                                                        
 		vm.treeCity.initClicks = function() {
-			vm.click(this.ascii, 'treeGovernment', 92, 6, 12, 7);
-			vm.click(this.ascii, 'slums', 83, 15, 40, 5);
-			vm.click(this.ascii, 'treeSecret', 66, 8, 2, 2);
-			vm.click(this.ascii, 'treeInn', 40, 9, 7, 3);
-			vm.click(this.ascii, 'treeShop', 12, 9, 7, 3);
+			vm.click(this, 'test', 'treeGovernment', 92, 6, 12, 7);
+			vm.click(this, 'test', 'slums', 83, 15, 40, 5);
+			vm.click(this, '','treeSecret', 66, 8, 2, 2);
+			vm.click(this, 'test', 'treeInn', 40, 9, 7, 3);
+			vm.click(this, 'test', 'treeShop', 12, 9, 7, 3);
 		}
 
 		vm.treeGovernment = new vm.Location;
+		vm.treeGovernment.name = 'Tree Government';
 		vm.treeGovernment.ascii = ['tree government'];
 		vm.treeGovernment.prev = 'treeCity';
 
 		vm.treeSecret = new vm.Location;
+		vm.treeSecret.name = 'Tree secret';
 		vm.treeSecret.ascii = ['tree secret'];
 		vm.treeSecret.prev = 'treeCity';
 
 		vm.treeShop = new vm.Location;
+		vm.treeShop.name = 'Tree Shop';
 		vm.treeShop.ascii = ['tree shop'];
 		vm.treeShop.prev = 'treeCity';
 
 		vm.treeInn = new vm.Location;
+		vm.treeInn.name = 'Tree Inn';
 		vm.treeInn.ascii = ['tree inn'];
 		vm.treeInn.prev = 'treeCity';
 
 		vm.treeSlums = new vm.Location;
 		vm.treeSlums.name = 'Tree Slums';
 		vm.treeSlums.initClicks = function() {
-			vm.click(this.ascii, 'slumsBum', 105, 12, 5, 4);
-			vm.click(this.ascii, 'slumsInn', 64, 11, 6, 5);
-			vm.click(this.ascii, 'slumsThugs', 42, 11, 13, 5);
+			vm.click(this, 'slumsBum', 105, 12, 5, 4);
+			vm.click(this, 'slumsInn', 64, 11, 6, 5);
+			vm.click(this, 'slumsThugs', 42, 11, 13, 5);
 		}
 		vm.treeSlums.prev = 'treeCity';
 		vm.treeSlums.ascii = ["            `                              ////\\\\\\\\                                                   ///\\\\\\                                      /////\\\\\\\\\\              ",                                                      
@@ -95,7 +150,7 @@
 								"             `                            /////\\\\\\\\\                                                //////\\\\\\\\\\\\\\                                 //////\\\\\\\\\\\\\\            ",                                                      
 								"           ___                             ////\\\\\\\\                                                //////\\\\\\\\\\\\                              ///////////\\\\\\\\\\\\\\\\\\         ",                                                      
 								"          |   |                            ////\\\\\\\\                                              ////////\\\\\\\\\\\\\\\\\\                            //////////\\\\\\\\\\\\\\\\\\         ",                                                      
-								"       ___|   |________________________ ///////\\\\\\\\\\\\\      _______________________________________///////\\\\\\\\\\\\\\\\                           ////////////\\\\\\\\\\\\\\\\\\\\\\\\      ",                                                                                           
+								"       ___|   |________________________ ///////\\\\\\\\\\\\\       ______________________________________///////\\\\\\\\\\\\\\\\                           ////////////\\\\\\\\\\\\\\\\\\\\\\\\      ",                                                                                           
 								"      /         ~~~~~~                 \\///////\\\\\\\\\\\\      /            ~~~~                       \\/////\\\\\\\\\\\\\\_____________________________///////////\\\\\\\\\\\\\\\\\\\\\\       ",                                                            
 								"     /    ~~~                ~~~~       \\//////\\\\\\\\\\\\\\\\   /   ~~~~                           ~~~    \\////\\\\\\\\\\\\/    ~~                    //////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\   ",                                                           
 								"    /____________________________________\\   |  |        /___________________________________________\\  | |   /____________________________/////////////\\\\\\\\\\\\\\\\\\\\\\\\\\     ",                                                            
@@ -114,26 +169,30 @@
                                                                                                                                                                                                                            
 
 		vm.treeSlumsInn = new vm.Location;
+		vm.treeSlumsInn.name = 'Tree Slums Inn';
 		vm.treeSlumsInn.ascii = ['tree slums inn'];
 		vm.treeSlumsInn.prev = 'slums';
 
 		vm.treeSlumsThugs = new vm.Location;
+		vm.treeSlumsThugs.name = 'Tree Slums Thugs';
 		vm.treeSlumsThugs.ascii = ['tree slums thugs'];
 		vm.treeSlumsThugs.prev = 'slums';
 
 		vm.treeSlumsBum = new vm.Location;
+		vm.treeSlumsBum.name = 'Tree Slums Bum';
 		vm.treeSlumsBum.ascii = ['tree slums bum'];
 		vm.treeSlumsBum.prev = 'slums';
 
-		vm.locationDictionary['treeCity'] = this.treeCity;
-			vm.locationDictionary['treeSecret'] = this.treeSecret;
-			vm.locationDictionary['treeGovernment'] = this.treeGovernment;
-			vm.locationDictionary['treeInn'] = this.treeInn;
-			vm.locationDictionary['treeShop'] = this.treeShop;
-			vm.locationDictionary['slums'] = this.treeSlums;
-				vm.locationDictionary['slumsInn'] = this.treeSlumsInn;
-				vm.locationDictionary['slumsThugs'] = this.treeSlumsThugs;
-				vm.locationDictionary['slumsBum'] = this.treeSlumsBum
+		vm.locationDictionary['mainMap'] = this.mainMap;
+			vm.locationDictionary['treeCity'] = this.treeCity;
+				vm.locationDictionary['treeSecret'] = this.treeSecret;
+				vm.locationDictionary['treeGovernment'] = this.treeGovernment;
+				vm.locationDictionary['treeInn'] = this.treeInn;
+				vm.locationDictionary['treeShop'] = this.treeShop;
+				vm.locationDictionary['slums'] = this.treeSlums;
+					vm.locationDictionary['slumsInn'] = this.treeSlumsInn;
+					vm.locationDictionary['slumsThugs'] = this.treeSlumsThugs;
+					vm.locationDictionary['slumsBum'] = this.treeSlumsBum
 
 
 		vm.switchLocation = function() {

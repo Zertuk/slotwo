2e24e477e8224ab9e1caced80d358c1f37299593
Justin <zertukis@gmail.com>
2015-12-03 23:43:36 -0600

    added resources module

---
+++ src/client/app/resources/resources.service.js
@@ -0,0 +1,13 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('app.resources')
+        .service('resourcesService', resourcesService);
+
+    // Service.$inject = ['dependencies'];
+
+    function resourcesService(dependencies) {
+
+    }
+})();

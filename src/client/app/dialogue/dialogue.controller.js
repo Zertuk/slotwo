7c97b007fce83c5834fba9a2da1355b9726f0ba1
Justin <zertukis@gmail.com>
2016-01-02 15:31:38 -0600

    cleanup, remove console.logs and old code

--- src/client/app/dialogue/dialogue.controller.js
+++ src/client/app/dialogue/dialogue.controller.js
@@ -9,32 +9,27 @@
 
     /* @ngInject */
     function DialogueController(dialogueService, progressService, mainService) {
-        console.log(mainService);
         var vm = this;
         dialogueService.initAllDialogues();
+        activate();
+
+
         vm.switchDialogue = function(input, master) {
             vm.currentMaster = dialogueService[master];
             vm.currentDialogue = vm.currentMaster.dialogue[input];
             vm.locationText = vm.currentDialogue.text;
             special();
-        }
+        };
 
         vm.switchLevel = function(level) {
             vm.currentLocation = mainService.switchLevel(level);
-        }
+        };
 
         function special() {
             if (typeof vm.currentDialogue.special !== 'undefined') {
                 vm.currentDialogue.special();
-                console.log('special');
-            }
-            else {
-                console.log(vm.currentDialogue);
             }
         }
-
-
-        activate();
 
         ////////////////
 
@@ -42,7 +37,6 @@
         function activate() {
             var location = mainService.currentLocation;
             var initDialogue = location.dialogue;
-            console.log(mainService.currentLocation);
             var key = initDialogue.initDialogue();
             vm.currentDialogue = initDialogue.dialogue[key];
             vm.locationText = vm.currentDialogue.text;

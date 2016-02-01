839bdc8f2da70bf45113d634cad89b25f9502eb6
Justin <zertukis@gmail.com>
2016-02-01 03:09:53 -0600

    logger hide

--- src/client/app/blocks/logger/logger.js
+++ src/client/app/blocks/logger/logger.js
@@ -25,23 +25,23 @@
         /////////////////////
 
         function error(message, data, title) {
-            toastr.error(message, title);
-            $log.error('Error: ' + message, data);
+            // toastr.error(message, title);
+            // $log.error('Error: ' + message, data);
         }
 
         function info(message, data, title) {
-            toastr.info(message, title);
-            $log.info('Info: ' + message, data);
+            // toastr.info(message, title);
+            // $log.info('Info: ' + message, data);
         }
 
         function success(message, data, title) {
-            toastr.success(message, title);
-            $log.info('Success: ' + message, data);
+            // toastr.success(message, title);
+            // $log.info('Success: ' + message, data);
         }
 
         function warning(message, data, title) {
-            toastr.warning(message, title);
-            $log.warn('Warning: ' + message, data);
+            // toastr.warning(message, title);
+            // $log.warn('Warning: ' + message, data);
         }
     }
 }());
